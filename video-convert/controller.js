5ea842656631b89140a962e229d48bc2424d81a3
kmiftahov <k.miftahov@cian.ru>
2020-06-24 21:29:31 +0300

    fix path to file

--- video-convert/controller.js
+++ video-convert/controller.js
@@ -2,6 +2,6 @@ const {videoConvertService} = require("./service");
 
 exports.videoConvertController = async (request, response) => {
   const { file } = request.body;
-  await videoConvertService(file);
-  return response.status(200).send('ok');
+  const data = await videoConvertService(file);
+  return response.status(200).send(data);
 };
