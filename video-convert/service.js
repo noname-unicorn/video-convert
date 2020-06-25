a7f600dbed3d8f567347abfe35e9492544f9f649
kmiftahov <k.miftahov@cian.ru>
2020-06-25 07:12:42 +0300

    fix path to file

--- video-convert/service.js
+++ video-convert/service.js
@@ -2,18 +2,31 @@ const ffmpeg = require('fluent-ffmpeg');
 
 exports.videoConvertService = async () => {
 
-  return new Promise((resolve, reject) => {
-    return ffmpeg.ffprobe('video-convert/test.mp4', (error, videoInfo) => {
-      if (error) {
-        return reject(error);
-      }
-
-      const { duration, size } = videoInfo.format;
-      console.log({duration, size});
-      return resolve({
-        size,
-        durationInSeconds: Math.floor(duration),
-      });
+  // return new Promise((resolve, reject) => {
+  //   return ffmpeg.ffprobe('video-convert/test.mp4', (error, videoInfo) => {
+  //     if (error) {
+  //       return reject(error);
+  //     }
+  //
+  //     const { duration, size } = videoInfo.format;
+  //     console.log({duration, size});
+  //     return resolve({
+  //       size,
+  //       durationInSeconds: Math.floor(duration),
+  //     });
+  //   })
+  // });
+  ffmpeg('video-convert/test.mp4')
+    .videoCodec('libx264')
+    .audioCodec('libmp3lame')
+    .size('320x240')
+    .on('error', function(err) {
+      console.log('An error occurred: ' + err.message);
+      return err.message
     })
-  });
+    .on('end', function() {
+      console.log('Processing finished !');
+      return 'Success';
+    })
+    .save('/path/to/output.mp4');
 };
